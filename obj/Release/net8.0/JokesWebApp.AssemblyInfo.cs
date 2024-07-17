0e988ae4a6160de3fe08833216dff2d46d0df108
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-07-17 23:13:08 +0400

    chore: add commit

--- obj/Release/net8.0/JokesWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/JokesWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-JokesWebApp-3af32bf6-6e32-4acf-924d-74a6d16d897e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesWebApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e3e03bf7392e74fc2f0a4be4a79557504bd66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesWebApp")]
