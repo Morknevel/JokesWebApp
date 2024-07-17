62cbae2c787320192ac4ea5643724a7215e8c4b4
Morknevel <lukakirthadze@gmail.com>
2024-07-17 22:37:59 +0400

    Normalize all the line endings

---
+++ Models/Jokes.cs
@@ -0,0 +1,9 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
