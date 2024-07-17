60aab72614e4dae5a0c2bd77fa34074346107e0e
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-07-17 23:35:58 +0400

    chavaswore

--- obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e3e03bf7392e74fc2f0a4be4a79557504bd66d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e988ae4a6160de3fe08833216dff2d46d0df108")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
